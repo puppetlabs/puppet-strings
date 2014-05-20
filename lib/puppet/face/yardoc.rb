3ad750d103e6904a14941d6c8d6c230df55d70d5
Charlie Sharpsteen <source@sharpsteen.net>
2014-05-19 22:24:03 -0700

    Add feature guards for necessary requirements
    Add `Puppet.feature` guards for the `yard` and `rgen` libraries.

--- lib/puppet/face/yardoc.rb
+++ lib/puppet/face/yardoc.rb
@@ -1,5 +1,4 @@
 require 'puppet/face'
-require 'puppet/pops'
 
 Puppet::Face.define(:yardoc, '0.0.1') do
 
@@ -7,6 +6,17 @@ Puppet::Face.define(:yardoc, '0.0.1') do
     default
 
     when_invoked do |*args|
+
+      unless Puppet.features.yard?
+        raise RuntimeError, "The 'yard' gem must be installed in order to use this face."
+      end
+
+      if Puppet.features.rgen?
+        require 'puppet/pops'
+      else
+        raise RuntimeError, "The 'rgen' gem must be installed in order to use this face."
+      end
+
       parser = Puppet::Pops::Parser::Parser.new()
       parse_result = parser.parse_file(args[0])
 
