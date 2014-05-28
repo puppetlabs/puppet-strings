787fca8ebcd5a8a8fac0562585fc865ae6dce32c
Charlie Sharpsteen <source@sharpsteen.net>
2014-05-27 18:51:25 -0700

    Implement inheritance tree for host classes
    Documentation pages for host classes now show the full inheritance tree instead
    of just the immediate parent.

--- lib/puppetx/yardoc/yard/code_objects/host_class_object.rb
+++ lib/puppetx/yardoc/yard/code_objects/host_class_object.rb
@@ -33,5 +33,20 @@ module Puppetx::Yardoc::YARD::CodeObjects
       yield(obj) if block_given?
       obj
     end
+
+    def inheritance_tree
+      if parent_class.is_a?(HostClassObject)
+        # Cool. We got a host class. Return self + parent inheritance tree.
+        [self] + parent_class.inheritance_tree
+      elsif parent_class.is_a?(YARD::CodeObjects::Proxy)
+        # We have a reference to a parent that has not been created yet. Just
+        # return it.
+        [self, parent_class]
+      else
+        # Most likely no parent class. But also possibly an object that we
+        # shouldn't inherit from. Just return self.
+        [self]
+      end
+    end
   end
 end
