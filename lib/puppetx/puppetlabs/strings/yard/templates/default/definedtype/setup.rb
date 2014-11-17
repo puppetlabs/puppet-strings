de3c1e776d5ae870af033e00f2c76261f1db9851
Hailee Kenney <hailee@puppetlabs.com>
2014-11-17 11:03:57 -0800

    (PDOC-17) Add support for YARD tags in puppet code
    Prior to this commit, most of the HTML created for documenting
    puppet code was auto-generated by YARD, meaning that it was not
    always presented in a way that was consistent or easy to read.
    
    Now we are explicitly defining the HTML and styling that we want
    to use when generating documentation for defined types and classes.
    This means that we can define exactly how we want the output to look
    so that is more compatible with puppet code.

--- lib/puppetx/puppetlabs/strings/yard/templates/default/definedtype/setup.rb
+++ lib/puppetx/puppetlabs/strings/yard/templates/default/definedtype/setup.rb
@@ -1,10 +1,41 @@
 include T('default/module')
 
 def init
-  sections :header, :box_info, :pre_docstring, T('docstring'), :parameter_details
+  sections :header, :box_info, :pre_docstring, :docstring, :parameter_details
 end
 
 def parameter_details
   return if object.parameters.empty?
+
+  param_tags = object.tags.find_all{ |tag| tag.tag_name == "param"}
+  params = object.parameters
+  @param_details = []
+
+  params.zip(param_tags).each do |param, tag|
+    description = tag.nil? ? nil : tag.text
+    param_types = tag.nil? ? nil : tag.types
+    @param_details.push({:name => param[0], :module => param[1], :desc => description, :types => param_types})
+  end
+
   erb(:parameter_details)
+end
+
+def docstring
+  examples = Hash.new
+  example_tags = object.tags.find_all { |tag| tag.tag_name == "example" }
+  example_tags.each do |example|
+    examples["#{example.name}"] = example.text
+  end
+
+  return_tag = object.tags.find { |tag| tag.tag_name == "return"}
+  return_text = return_tag.nil? ? nil : return_tag.text
+  return_types = return_tag.nil? ? nil : return_tag.types
+  return_details = (return_text.nil? && return_types.nil?) ? nil : [return_text, return_types]
+
+  since_tag = object.tags.find { |tag| tag.tag_name == "since"}
+  since_text = since_tag.nil? ? nil : since_tag.text
+
+  @class_details = {:name => object.name, :desc => object.docstring, :examples => examples, :since => since_text, :return => return_details}
+
+  erb(:docstring)
 end
