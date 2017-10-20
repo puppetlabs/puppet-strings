e4ef0ed2f873778efd9307ad0b8246956cd4b360
Jesse Scott <jesse@puppetlabs.com>
2017-10-20 14:31:51 -0700

    (MAINT) Fix up rubocop settings and clean up inconsistent indentation

--- spec/acceptance/emit_json_options.rb
+++ spec/acceptance/emit_json_options.rb
@@ -5,37 +5,37 @@ require 'json'
 include PuppetStrings::Acceptance::Util
 
 describe 'Emitting JSON' do
-expected = {
-  "puppet_classes" => [],
-  "defined_types" => [],
-  "resource_types" => [],
-  "providers" => [],
-  "puppet_functions" => [
-    "name" => "function3x",
-    "file" => "/etc/puppet/modules/test/lib/puppet/parser/functions/function3x.rb",
-    "line" => 1,
-    "type" => "ruby3x",
-    "signatures" => [
-      {
-        "signature" =>"function3x()",
-        "docstring" => {
-          "text" => "This is the function documentation for `function3x`",
-          "tags" => [
-            {
-              "tag_name"=>"return",
-              "text"=>"",
-              "types"=>["Any"]
-            }
-          ]
-        }
-      },
-    ],
-    "docstring" => {
-      "text" => "This is the function documentation for `function3x`",
-      "tags" => ["tag_name" => "return", "text" => "", "types" => ["Any"]]},
-      "source" => "Puppet::Parser::Functions.newfunction(:function3x, :doc => \"This is the function documentation for `function3x`\") do |args|\nend"
-  ]
-}
+  expected = {
+    "puppet_classes" => [],
+    "defined_types" => [],
+    "resource_types" => [],
+    "providers" => [],
+    "puppet_functions" => [
+      "name" => "function3x",
+      "file" => "/etc/puppet/modules/test/lib/puppet/parser/functions/function3x.rb",
+      "line" => 1,
+      "type" => "ruby3x",
+      "signatures" => [
+        {
+          "signature" =>"function3x()",
+          "docstring" => {
+            "text" => "This is the function documentation for `function3x`",
+            "tags" => [
+              {
+                "tag_name"=>"return",
+                "text"=>"",
+                "types"=>["Any"]
+              }
+            ]
+          }
+        },
+      ],
+      "docstring" => {
+        "text" => "This is the function documentation for `function3x`",
+        "tags" => ["tag_name" => "return", "text" => "", "types" => ["Any"]]},
+        "source" => "Puppet::Parser::Functions.newfunction(:function3x, :doc => \"This is the function documentation for `function3x`\") do |args|\nend"
+    ]
+  }
 
   it 'should emit JSON to stdout when using the --emit-json-stdout option' do
     test_module_path = get_test_module_path(master, /Module test/)
@@ -49,7 +49,7 @@ expected = {
     test_module_path = get_test_module_path(master, /Module test/)
     tmpfile = master.tmpfile('json_output.json')
     on master, puppet('strings', 'generate', "--emit-json #{tmpfile}", "#{test_module_path}/lib/puppet/parser/functions/function3x.rb")
-      output = read_file_on(master, tmpfile)
-      expect(JSON.parse(output)).to eq(expected)
+    output = read_file_on(master, tmpfile)
+    expect(JSON.parse(output)).to eq(expected)
   end
 end
