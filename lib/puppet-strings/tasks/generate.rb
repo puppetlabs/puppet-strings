784c32c979a101c2e722a0d934aead133b8bdcfa
Eric Putnam <putnam.eric@gmail.com>
2018-02-26 15:36:28 -0800

    (maint) update rake tasks

--- lib/puppet-strings/tasks/generate.rb
+++ lib/puppet-strings/tasks/generate.rb
@@ -3,7 +3,7 @@ require 'puppet-strings'
 # Implements the strings:generate task.
 namespace :strings do
   desc 'Generate Puppet documentation with YARD.'
-  task :generate, :patterns, :debug, :backtrace, :markup, :json, :yard_args do |t, args|
+  task :generate, :patterns, :debug, :backtrace, :markup, :json, :markdown, :yard_args do |t, args|
     patterns = args[:patterns]
     patterns = patterns.split if patterns
     patterns ||= PuppetStrings::DEFAULT_SEARCH_PATTERNS
@@ -17,6 +17,7 @@ namespace :strings do
 	# rubocop:disable Style/PreferredHashMethods
 	# `args` is a Rake::TaskArguments and has no key? method
     options[:json] = args[:json] if args.has_key? :json
+    options[:markdown] = args[:markdown] if args.has_key? :markdown
     options[:yard_args] = args[:yard_args].split if args.has_key? :yard_args
 	# rubocop:enable Style/PreferredHashMethods
 
