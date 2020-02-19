37498554687771cebbc7538abe33bc269b210c02
Glenn Sarti <glennsarti@users.noreply.github.com>
2020-02-19 09:26:58 +0800

    (maint) Add markdown lint checking to markdown tests
    Previously the markdown generated by Puppet-Strings may contain errors which
    markdown lint programs would flag.  This commit adds the ability to run markdown
    lint on a per test basis and adds pending tests to show that the markdown output
    does indeed need some work.
    
    Note that this commit does not fix the errors.  That will come in later commits.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,3 +47,45 @@ RSpec.configure do |config|
     YARD::Registry.clear
   end
 end
+
+def mdl_available
+  @mdl_available ||= !Gem::Specification.select { |item| item.name.casecmp('mdl').zero? }.empty?
+end
+
+def lint_markdown(content)
+  return [] unless mdl_available
+  require 'mdl'
+
+  ruleset = MarkdownLint::RuleSet.new
+  ruleset.load_default
+
+  # All rules
+  style = MarkdownLint::Style.load('all', ruleset.rules)
+
+  # Create a document
+  doc = MarkdownLint::Doc.new(content, false)
+
+  # Run the rules
+  violations = []
+  ruleset.rules.each do |id, rule|
+    error_lines = rule.check.call(doc)
+    next if error_lines.nil? or error_lines.empty?
+    # record the error
+    error_lines.each do |line|
+      line += doc.offset # Correct line numbers for any yaml front matter
+      violations << "#{filename}:#{line}: #{id} #{rule.description}"
+    end
+  end
+  violations
+end
+
+RSpec::Matchers.define :have_no_markdown_lint_errors do
+  match do |actual|
+    @violations = lint_markdown(actual)
+    @violations.empty?
+  end
+
+  failure_message do |actual|
+    "expected that #{actual.length > 80 ? actual.slice(0,80).inspect + '...' : actual.inspect} would have no markdown lint errors but got #{@violations.join("\n")}"
+  end
+end
