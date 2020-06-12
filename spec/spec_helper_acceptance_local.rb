8cd8c07119c73a90a50fa6fc1bf2a113362c491e
David Schmitt <david.schmitt@puppet.com>
2020-06-12 11:59:19 +0100

    (maint) use condensed litmus spec_helper_acceptance; fixup tests

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -1,5 +1,5 @@
 def sut_module_path(module_regex)
-  modules = JSON.parse(PuppetLitmus::PuppetHelpers.run_shell('puppet module list --render-as json').stdout)
+  modules = JSON.parse(run_shell('puppet module list --render-as json').stdout)
   test_module_info = modules['modules_by_path'].values.flatten.find { |mod_info| mod_info =~ module_regex }
   test_module_info.match(/\(([^)]*)\)/)[1]
 end
