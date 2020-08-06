6a5014914bbf4f7b2eb8914f07a9b991141cf4e2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-08-06 17:36:37 +0200

    Handle a missing description gracefully
    b5da040babf4bdbbcfd9bfabdb210041ccfa8ce3 introduced word_wrap which
    assumes text is a string. However, this is not true and not every
    resource type has a description.

--- lib/puppet-strings/markdown/base.rb
+++ lib/puppet-strings/markdown/base.rb
@@ -170,6 +170,7 @@ module PuppetStrings::Markdown
     end
 
     def word_wrap(text, line_width: 120, break_sequence: "\n")
+      return unless text
       text.split("\n").collect! do |line|
         line.length > line_width ? line.gsub(/(.{1,#{line_width}})(\s+|$)/, "\\1#{break_sequence}").strip : line
       end * break_sequence
