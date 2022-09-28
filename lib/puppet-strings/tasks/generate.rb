223ef05bcf31e4f3f3ee385633ef5afb58276d56
Craig Gumbley <craiggumbley@gmail.com>
2022-09-28 09:21:37 +0100

    (MAINT) Rubocop - (lib) fixes for tasks

--- lib/puppet-strings/tasks/generate.rb
+++ lib/puppet-strings/tasks/generate.rb
@@ -5,7 +5,7 @@ require 'puppet-strings'
 # Implements the strings:generate task.
 namespace :strings do
   desc 'Generate Puppet documentation with YARD.'
-  task :generate, [:patterns, :debug, :backtrace, :markup, :json, :markdown, :yard_args] do |t, args|
+  task :generate, [:patterns, :debug, :backtrace, :markup, :json, :markdown, :yard_args] do |_t, args|
     patterns = args[:patterns]
     patterns = patterns.split if patterns
     patterns ||= PuppetStrings::DEFAULT_SEARCH_PATTERNS
@@ -16,7 +16,7 @@ namespace :strings do
       markup: args[:markup] || 'markdown',
     }
 
-    raise("Error: Both JSON and Markdown output have been selected. Please select one.") if args[:json] == 'true' && args[:markdown] == 'true'
+    raise('Error: Both JSON and Markdown output have been selected. Please select one.') if args[:json] == 'true' && args[:markdown] == 'true'
 
     # rubocop:disable Style/PreferredHashMethods
     # Because of Ruby, true and false from the args are both strings and both true. Here,
@@ -28,16 +28,14 @@ namespace :strings do
     # @param [Symbol] possible format option
     # @return nil
     def parse_format_option(args, options, format)
-      if args.has_key? format
-        options[format] = args[format] == 'false' || args[format].empty? ? false : true
-        if options[format]
-          options[:path] = args[format] == 'true' ? nil : args[format]
-        end
-      end
+      return unless args.has_key? format
+      options[format] = args[format] == 'false' || args[format].empty? ? false : true
+      return unless options[format]
+      options[:path] = args[format] == 'true' ? nil : args[format]
     end
     # rubocop:enable Style/PreferredHashMethods
 
-    %i[json markdown].each { |format| parse_format_option(args, options, format) }
+    [:json, :markdown].each { |format| parse_format_option(args, options, format) }
 
     warn('yard_args behavior is a little dodgy, use at your own risk') if args[:yard_args]
     options[:yard_args] = args[:yard_args].split if args.key? :yard_args
@@ -47,7 +45,7 @@ namespace :strings do
 
   namespace :generate do
     desc 'Generate Puppet Reference documentation.'
-    task :reference, [:patterns, :debug, :backtrace] do |t, args|
+    task :reference, [:patterns, :debug, :backtrace] do |_t, args|
       Rake::Task['strings:generate'].invoke(args[:patterns], args[:debug], args[:backtrace], nil, 'false', 'true')
     end
   end
